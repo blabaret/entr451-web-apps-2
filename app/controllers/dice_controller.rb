f65b41c856a7a3237f13e5e3d9e799d8c40c0cc4
blabaret <dennis.xiayue@kellogg.northwestern.edu>
2023-02-14 20:02:04 +0000

    Complete dice

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,3 +1,15 @@
 class DiceController < ApplicationController
 
+
+    #def index
+    # render the file in /views/dice/index.html.erb
+    #end
+
+    def index
+        #die 1 = "something"
+        @die1 = rand(1..6)
+        @die2 = rand(1..6)
+        @total = @die1 + @die2
+    end
+
 end
