416a798118671586ef0f1ba338ee18e37852d4d5
blabaret <dennis.xiayue@kellogg.northwestern.edu>
2023-02-15 00:05:04 +0000

    Final cahnges

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,27 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+    @posts = Post.new
+  end
+
+  def show
+    @company = Company.find_by({ "id" => params["id"] })
+  end
+
+  def create
+    @posts = Post.new
+    @posts["author"] = params["posts"]["author"]
+    @posts["body"] = params["posts"]["body"]
+    @posts["image"] = params["posts"]["image"]
+    @posts.save
+    redirect_to "/posts"
+  end
+
+  def destroy
+    #find post find_by
+    #post.destroy
+    #redirect
+  end
+
 end
